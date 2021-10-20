cbfde2f22f2f3a43d19cf38cda14c512c144f6e9
Sora Akashi <sora.air.akashi@gmail.com>
2021-10-20 12:26:04 +0900

    2021 1020

--- 1020/list4.py
+++ 1020/list4.py
@@ -1,1 +1,16 @@
 n=input("整数を入力してください")
+#入力した値を整数にしないといけないnum = int(n)
+numbres = range(1,int(n) + 1)
+
+total = 0
+average = 0
+num = int(n)
+i = 0
+
+while i <= num:
+    total += i
+    i += 1
+average = total / num
+
+print ("1~{n}までの合計:{total}")
+print("平均:{average}")
