3cd0c1f1cb8ded67b2916af3147239ba6281f640
Sora Akashi <sora.air.akashi@gmail.com>
2021-10-27 12:01:27 +0900

    1027 aka

--- 1027/math.py
+++ 1027/math.py
@@ -3,5 +3,5 @@ input_angle = float(input('角度を入力してください（度）：'))
 
 angle = input_angle * math.pi /180
 
-print(f'{imput_angle}度 -> {angle} ラジアン')
+print(f'{input_angle}度 -> {angle} ラジアン')
 print(f'sin({input_angle} => {math.sin(angle)})')
