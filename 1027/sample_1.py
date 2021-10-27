3748f2701367c7299432aed2d108db93e14224b4
Sora Akashi <sora.air.akashi@gmail.com>
2021-10-27 10:21:45 +0900

    1027 akashi

--- 1027/sample_1.py
+++ 1027/sample_1.py
@@ -1,8 +1,10 @@
 while True:
-    num = int(input("整数を入力してください"))
-    if(input/2 == 0){
-        print('偶数です')
-    }else{
-        print('奇数です')
-    }
-    break
+    num = int(input("整数を入力してください(終了 -> 0)"))
+    
+    if num == 0:
+        break
+
+    if num % 2 == 0:
+        print("偶数です。")
+    else:
+        print("奇数です。")
