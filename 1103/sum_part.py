0a757c5814ab57fa3c86fe318343e4741b1b04a4
Sora Akashi <sora.air.akashi@gmail.com>
2021-11-03 11:13:45 +0900

    1103 ak

--- 1103/sum_part.py
+++ 1103/sum_part.py
@@ -1,6 +1,7 @@
 input_num1 = int(input("数字:"))
 input_num2 = int(input("数字:"))
 
-
-
-for i in range():
+my_sum = 0
+for i in range(input_num1,input_num2+1):
+    my_sum += i
+print('{0}から{1}までの合計は{2}です'.format(input_num1, input_num2, my_sum))
