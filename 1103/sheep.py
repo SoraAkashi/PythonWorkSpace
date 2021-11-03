765bab7b16512dc30cf5dfceeb1568fffb694daf
Sora Akashi <sora.air.akashi@gmail.com>
2021-11-03 12:21:17 +0900

    1103 ak

--- 1103/sheep.py
+++ 1103/sheep.py
@@ -1,6 +1,15 @@
-a = 0
-print('何匹数えますか？')
-val = input()
-while a < val :
-    a += 1 
-    print('羊が{a}匹')
+import time
+
+while True:
+    max_num = int(input('何匹に見えますか？:'))
+    if max_num > 100:
+        print('多すぎます')
+        continue
+    else:
+        break
+
+my_count = 1
+while my_count <= max_num:
+    print('羊が{}匹'.format(my_count))
+    time.sleep(my_count/10)
+    my_count += 1
