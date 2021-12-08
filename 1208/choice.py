6e313754789d76bc45b48a1d989760b0f89f40e2
Sora Akashi <sora.air.akashi@gmail.com>
2021-12-08 12:41:05 +0900

    1208ak

--- 1208/choice.py
+++ 1208/choice.py
@@ -2,8 +2,18 @@ def odd():
     print("奇数") 
 def even(): 
     print("偶数") 
-def choice_func(number): 
+def choice_func(number):
+    func_dic = {1: odd, 0: even}
+    func = func_dic[number % 2]
     return func 
 # main 
 while True: 
     num = input("数字を入力してください。（0：終了）")
+    if num.isnumeric():
+        num = int(num)
+    else:
+        print("入力が正しくありません")
+        continue
+    if num == 0:
+        break
+    choice_func(num)() #even()
---
+++ 1208/lambda_1.py
@@ -0,0 +1,8 @@
+my_list = [1, 3, 5, 7, 2, 4, 6, 8] 
+#list 
+# 受け取ったイミテータを基にリストを作成する
+#map
+# 繰り返し処理をシンプルにする
+new_list = list(map(lambda x :x*10),my_list)
+print(my_list) 
+print(new_list)
